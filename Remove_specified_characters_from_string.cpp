b7bdae99d20b4d3fe2fc126411bcc3d66b306e55
Chengfei <chengfei0913@gmail.com>
2013-09-18 14:07:40 +0800

    description

--- Remove_specified_characters_from_string.cpp
+++ Remove_specified_characters_from_string.cpp
@@ -4,9 +4,14 @@ using namespace std;
 /*
 Problem Description:
 
-删除字符串中的“b”和“ac”，需要满足如下的条件：字符串只能遍历一次；不能够实用额外的空间。
-例如：acbac ==> ""；aaac ==> aa；ababac ==> aa；bbbbd ==> d。
-进一步思考：如何处理aaccac呢，需要做哪些改变呢？
+Delete "b" and "ac" from the string.
+Requirement: One time traversal, no more space.
+For example:
+            "acbac'  ==> "";
+            "aaac"   ==> "aa";
+            "ababac" ==> "aa";
+            "bbbbd"  ==> "d".
+Further：How about "aaccac"?
 
 */
 
