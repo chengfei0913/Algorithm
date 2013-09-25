af5c63ad423ada03b2f71a483bbeb24b947d3a28
fly <chengfei0913@gmail.com>
2013-09-25 23:04:09 +0800

    unfinished code

--- Longest_Palindromic_Substring.cpp
+++ Longest_Palindromic_Substring.cpp
@@ -1,8 +1,34 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+
+char* longest_palindromic_substring(char* s)
+{
+    if(s == NULL)
+        return NULL;
+
+    int len = strlen(s);
+    if(len <= 1)
+        return s;
+
+    char* new_s = (char*)malloc((2*len+4)*sizeof(char));
+    new_s[0] = '^';
+    new_s[1] = '#';
+    for(int i = 0; i < len; ++i)
+    {
+        new_s[2*i+2] = s[i];
+        new_s[2*i+3] = '#';
+    }
+    new_s[2*len+2] = '$';
+    new_s[2*len+3] = '\0';
+
+    return new_s;
+}
 
 int main()
 {
+    char s[] = "abcddcbc";
+    char* lps = longest_palindromic_substring(s);
+    cout << lps << endl;
 
     cin.get();
     return 0;
